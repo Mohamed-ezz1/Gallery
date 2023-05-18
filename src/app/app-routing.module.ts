fca53f4f62e130a6f82310fe998c34b165131d5b
Mohamed Ezz Elarab <moh.ezzelarab@gmail.com>
2023-05-18 20:41:46 +0300

    Routing to the userpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersPageComponent } from './components/users-page/users-page.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PhotosComponent } from './components/photos/photos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: UsersPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
