da60777dc72f520ef39a7372d54c03def8742974
Mohamed Ezz Elarab <moh.ezzelarab@gmail.com>
2023-05-18 20:36:47 +0300

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UsersPageComponent } from './components/users-page/users-page.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UsersPageComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
