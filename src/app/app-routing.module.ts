e8d7d0e1d74185482be25a6b0d6aa11b47776781
Mohamed Ezz Elarab <moh.ezzelarab@gmail.com>
2023-05-28 22:57:38 +0300

    Adding routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersPageComponent } from './components/users-page/users-page.component';
-import { AlbumsComponent } from './components/albums/albums.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { PhotosComponent } from './components/photos/photos.component';
 
-const routes: Routes = [{ path: '', component: UsersPageComponent }];
+
+const routes: Routes = [{ path: '', component: UsersPageComponent },
+{ path: "users/:id", component: UserDetailsComponent },
+{ path: "albums/:id", component: PhotosComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
